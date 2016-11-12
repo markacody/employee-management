1588f25a0dd6df675884e59a05afced96e7e451d
Matthew Tutak <mtutak91@gmail.com>
2016-11-12 10:57:53 -0600

    updating firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+console.log("connected");
+
+// Initialize Firebase
+var config = {
+   apiKey: "AIzaSyCQ0sysWIKKDflw96M2FMZ6HVPcEXmRNQg",
+   authDomain: "employee-billable-hours.firebaseapp.com",
+   databaseURL: "https://employee-billable-hours.firebaseio.com",
+   storageBucket: "employee-billable-hours.appspot.com",
+   messagingSenderId: "262395923194"
+ };
+ firebase.initializeApp(config);
